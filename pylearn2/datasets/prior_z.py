86cafce8480514a13aab5de9426408aa49f41c81
Jean Pouget-Abadie <jean.pougetabadie@gmail.com>
2014-05-28 14:06:17 -0400

    ba

--- pylearn2/datasets/prior_z.py
+++ pylearn2/datasets/prior_z.py
@@ -5,7 +5,7 @@ import numpy
 class BernoulliPrior(DenseDesignMatrix):
     def __init__(self, num_z, dim_size):
         self.shape = (num_z, dim_size)
-        assert num_z < 500  # Memory overflow otherwise (dim_size * num_z * n_vis * 4 bytes)
+        assert num_z <= 500  # Memory overflow otherwise (dim_size * num_z * n_vis * 4 bytes)
         X = numpy.random.randint(2, size=(num_z, dim_size)).astype('float32')
         # self._iter_subset_class = resolve_iterator_class('shuffled_sequential')
         super(BernoulliPrior, self).__init__(X=X)
